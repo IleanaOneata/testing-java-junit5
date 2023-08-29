177538590b9f6820e0a5e18dcc9969b9830da6e1
Ileana Oneata <ileana.oneata@yahoo.com>
2023-08-29 21:41:33 +0300

    JUnit Grouped Assertions

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,33 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author ileanaoneata on 29.08.2023
+ */
+class PersonTest {
+
+    @Test
+    void groupedAssertions() {
+        // given
+        Person person = new Person(1l, "Joe", "Buck");
+
+        // then
+        assertAll("Test Props Set",
+                () -> assertEquals("Joe", person.getFirstName()),
+                () -> assertEquals( "Buck", person.getLastName()));
+    }
+
+    @Test
+    void groupedAssertionMsgs() {
+        // given
+        Person person = new Person(1l, "Joe", "Buck");
+
+        // then
+        assertAll("Test Props Set",
+                () -> assertEquals("Joe", person.getFirstName(), "First Name Failed"),
+                () -> assertEquals("Buck", person.getLastName(), "Last Name Failed"));
+    }
+}
