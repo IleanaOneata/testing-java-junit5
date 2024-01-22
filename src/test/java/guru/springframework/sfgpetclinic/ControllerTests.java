9e32043afd3caffe69aa8624a0bd76619839380a
Ileana Oneata <ileana.oneata@yahoo.com>
2024-01-22 20:15:46 +0200

    Using JUnit Default Test Methods

--- src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/ControllerTests.java
@@ -1,11 +1,19 @@
 package guru.springframework.sfgpetclinic;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
 /**
  * @author ileanaoneata on 19.01.2024
  */
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Tag("controllers")
 public interface ControllerTests {
+
+    @BeforeAll
+    default void beforeAll() {
+        System.out.println("Lets do something here");
+    }
 }
